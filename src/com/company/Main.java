b98a7f96fa1d5e8cded67ce083741cb786edf9b3
somihaseeb <somihaseeb@gmail.com>
2021-11-05 19:58:29 -0400

    my lambo car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        car newCar = new car();
+
+        newCar.name = "Lamboo1";
+        newCar.engineCapacity = "4456781";
+        newCar.doors = 71;
+        newCar.model = "aventador1";
+
+        System.out.println(newCar.model);
     }
 }
